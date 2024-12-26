93e4f7064511cd4b53375f51bd1a8dd3c0e40735
RelinaTiwari <relinatiwari.rt@gmail.com>
2024-12-26 17:04:53 +0530

    added 2 new files

---
+++ src/selenium/GitPushRequest.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class GitPushRequest {
+	
+	@Test
+	public void showPushFunctionalty()
+	{
+		System.out.println("This is how Git push works");
+	}
+	
+	@Test
+	public void showPushFunctionalty2()
+	{
+		System.out.println("This is how Git push works");
+	}
+	
+
+}
---
+++ src/selenium/GitPushRequest2.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class GitPushRequest2 {
+	
+	@Test
+	public void showPushFunctionalty()
+	{
+		System.out.println("This is how Git push works");
+	}
+	
+	@Test
+	public void showPushFunctionalty2()
+	{
+		System.out.println("This is how Git push works");
+	}
+	
+
+}
